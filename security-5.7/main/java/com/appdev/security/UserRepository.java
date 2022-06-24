8ed2af3d2266ba73ba28ae01869908b0ce67a049
appdev1449 <108109318+appdev1449@users.noreply.github.com>
2022-06-24 09:47:40 +0530

    Delete security-5.7 directory

--- security-5.7/main/java/com/appdev/security/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.appdev.security;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Integer>{
-	public User findByUserName(String userName);
-}
